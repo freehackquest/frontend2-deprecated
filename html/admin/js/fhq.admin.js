c0b5d9e352e83f1ecbca6df01edfa5ff6cabde45
Evgenii Sopov <mrseakg@gmail.com>
2018-02-07 23:53:54 +0700

    Added template for admin pages

---
+++ html/admin/js/fhq.admin.js
@@ -0,0 +1,57 @@
+if(!window.fhq) window.fhq = {};
+
+
+
+fhq.processParams = function(){
+	if(fhq.userinfo && fhq.userinfo.role != 'admin'){
+		fhq.ws.cleanuptoken();
+		fhq.showSignInForm();
+	}else{
+		console.log(fhq.userinfo);
+	}
+}
+
+fhq.showSignInForm = function(){
+	$('#signinErrorMessage').hide();
+	$('#signinEmailInput').unbind().bind('keyup', function(event){
+		if (event.keyCode == 13){
+			fhq.signin();
+		} else {
+			fhq.cleanupSignInMessages();
+		}
+	});
+	
+	$('#signinPasswordInput').unbind().bind('keyup', function(event){
+		if (event.keyCode == 13){
+			fhq.signin();
+		} else {
+			fhq.cleanupSignInMessages();
+		}
+	});
+	
+	$('#modalSignIn').modal('show');
+}
+
+fhq.cleanupSignInMessages = function() {
+	$('#signinErrorMessage').html('');
+	$('#signinErrorMessage').hide();
+}
+
+fhq.signin = function() {
+	var email = $("#signinEmailInput").val();
+	var password = $("#signinPasswordInput").val();
+	
+	fhq.ws.login({email: email,password: password}).done(function(r){
+		console.log(r);
+		if(r.user.role != 'admin'){
+			$('#signinErrorMessage').show();
+			$("#signinErrorMessage").html("This page allow only for admin");
+		}else{
+			$('#modalSignIn').modal('hide');
+			$('.modal-backdrop').hide();
+		}
+	}).fail(function(r){
+		$('#signinErrorMessage').show();
+		$("#signinErrorMessage").html(r.error);
+	})
+}

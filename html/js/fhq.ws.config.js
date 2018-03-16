fbbb2e6b305405366b267e7ae4e563dab5afce3e
Evgenii Sopov <mrseakg@gmail.com>
2018-03-17 02:56:44 +0700

    Changed defaults port

--- html/js/fhq.ws.config.js
+++ html/js/fhq.ws.config.js
@@ -2,5 +2,6 @@ if(!window.fhq) window.fhq = {};
 if(!window.fhq.ws) window.fhq.ws = {};
 
 fhq.ws.protocol = window.location.protocol == "https:" ? "wss:" : "ws:";
-fhq.ws.port = window.location.protocol == "https:" ? "4613" : "1234";
+// fhq.ws.port = window.location.protocol == "https:" ? "4613" : "1234";
+fhq.ws.port = window.location.protocol == "https:" ? "355" : "350";
 fhq.ws.hostname = window.location.hostname;

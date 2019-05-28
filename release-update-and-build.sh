45930e5108c8f19e8f21bbeb4da0993962fb4f6e
Evgenii Sopov <mrseakg@gmail.com>
2019-05-29 05:27:21 +0700

    Improved script for release

--- release-update-and-build.sh
+++ release-update-and-build.sh
@@ -4,3 +4,11 @@ git checkout package-lock.json
 git pull -p
 npm install
 npm run build
+if [ $1 -ne 0 ]; then
+    echo "Build failed"
+    exit
+else
+    echo "Build success"
+    rm -rf dist_new
+    cp -rf dist dist_new
+fi

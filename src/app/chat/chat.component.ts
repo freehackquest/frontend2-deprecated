75d644ccbcebefbcca3e741a9affe48a2b4ad788
Evgenii Sopov <mrseakg@gmail.com>
2019-06-06 20:39:23 +0700

    Implement list and send chat message

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, ViewChild, ElementRef } from '@angular/core';
 import { SpinnerService } from '../spinner.service';
 
 declare var fhq: any;
@@ -10,34 +10,60 @@ declare var fhq: any;
 })
 export class ChatComponent implements OnInit {
   dataList: Array<any> = [];
+  dataReverseList: Array<any> = [];
   errorMessage: string = null;
-
+  @ViewChild('yourMessage') yourMessage : ElementRef;
+  @ViewChild('chatMessages') private chatMessages: ElementRef;
+  
   constructor(
     private _spinnerService: SpinnerService,
     private _cdr: ChangeDetectorRef,
   ) { }
 
   ngOnInit() {
+    this.loadData();
   }
 
   loadData() {
     const _data = {}
     this._spinnerService.show();
-    fhq.ws.chat_latest_messages(_data)
+    fhq.chat_latest_messages(_data)
       .done((r: any) => this.successResponse(r))
       .fail((err: any) => this.errorResponse(err));
     // 
   }
 
+  sendMessage() {
+    const msg = this.yourMessage.nativeElement.value;
+    console.log(msg);
+    fhq.chat_send_message({
+      "type": "chat",
+      "message": msg
+    }).done(function(r: any) {
+        console.log('Success: ', r);
+    }).fail(function(err: any){
+        console.error('Error:', err);
+    });
+    this.yourMessage.nativeElement.value = '';
+  }
+
   successResponse(r: any) {
     console.log(r);
     this._spinnerService.hide();
-
     this.dataList = []
-    /*r.data.forEach((el: any) => {
+    this.dataReverseList = [];
+    r.data.forEach((el: any) => {
       this.dataList.push(el);
-    });*/
+    });
+    this.dataReverseList = this.dataList.reverse();
     this._cdr.detectChanges();
+    this.chatScrollToBottom();
+  }
+
+  chatScrollToBottom() {
+    try {
+      this.chatMessages.nativeElement.scrollTop = this.chatMessages.nativeElement.scrollHeight;
+    } catch(err) { }   
   }
 
   errorResponse(err: any) {

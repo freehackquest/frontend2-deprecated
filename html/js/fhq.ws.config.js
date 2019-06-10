aaf79b259a9c809c003cb9491161520859895080
Evgenii Sopov <mrseakg@gmail.com>
2019-06-11 02:25:16 +0700

    Prepare Knowledge Base menu

--- html/js/fhq.ws.config.js
+++ html/js/fhq.ws.config.js
@@ -4,10 +4,13 @@ if(!window.fhq.ws) window.fhq.ws = {};
 
 fhq.ws.base_url = 'ws://' + window.location.hostname + ":1234/api-ws/";
 
-if(window.location.protocol == "https:"){
+if (window.location.protocol == "https:") {
 	fhq.ws.base_url = "wss://" + window.location.hostname + ":4613/api-wss/";
 }
 
-if(window.location.hostname == "freehackquest.com"){
+if (window.location.hostname == "freehackquest.com") {
 	fhq.ws.base_url = 'wss://freehackquest.com/api-wss/';
 }
+
+// fhq.ws.base_url = 'ws://freehackquest.com/api-ws/';
+// fhq.ws.base_url = 'ws://localhost/api-ws/';

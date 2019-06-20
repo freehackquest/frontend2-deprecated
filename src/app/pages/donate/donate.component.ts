d7a53d3455840283051f8b130cebfc7065594dee
Evgenii Sopov <mrseakg@gmail.com>
2019-06-20 13:44:09 +0700

    Fixed loader on danate and page-not-found

--- src/app/pages/donate/donate.component.ts
+++ src/app/pages/donate/donate.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SpinnerService } from '../../services/spinner.service';
 
 @Component({
   selector: 'app-donate',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DonateComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _spinner: SpinnerService,
+  ) { }
 
   ngOnInit() {
+    this._spinner.hide();
   }
 
 }
--- src/app/pages/page-not-found/page-not-found.component.ts
+++ src/app/pages/page-not-found/page-not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SpinnerService } from '../../services/spinner.service';
 
 @Component({
   selector: 'app-page-not-found',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageNotFoundComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _spinner: SpinnerService,
+  ) { }
 
   ngOnInit() {
+    this._spinner.hide();
   }
 
 }
